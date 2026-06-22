cd6a75bd344babce8a2dc66e954226ba48e4cb3f
ghargelg-code26 <ghargelg@gmail.com>
2026-06-22 23:00:52 +0100

    first numpy programm

---
+++ jupyter_notebook/array_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+# my_list = [19,'female',27.9,0,True,'southwest',16884.924]
+my_list =  [ [536365,  17850,  6], [536366,  17850,  6], [536367, 13047, 32] ]
+my_arr = np.array(my_list)
+print(my_arr.dtype)
+words = np.array(['bacon', 'egg', 'spam'])
+print(words)
+arr = np.arange(start=1,stop=9,step=0.5)
+print(arr)
